97077585efe9cdc04193193144cf6e65ec00afed
Marvin Gravert <marvin.gravert@gmail.com>
2019-12-11 01:11:21 +0100

    Forgot To commit

---
+++ Assets/broadcastTransformation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class broadcastTransformation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Test;
+    GameObject obj;
+    void Start()
+    {
+        //print(gameObject.name);
+        obj = this.gameObject;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int randomNumber = Random.Range(0, 10);
+        string text = randomNumber.ToString();
+        Vector3 posi = this.transform.position;
+        Test.GetComponent<TextMesh>().text = posi.ToString();
+    }
+}
---
+++ Assets/changeText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //int randomNumber = Random.Range(0, 10);
+        //string text = randomNumber.ToString();
+        //GetComponent<TextMesh>().text = text;
+    }
+}
